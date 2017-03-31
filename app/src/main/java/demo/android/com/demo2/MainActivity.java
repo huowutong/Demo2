fe136aa43f7a41c2f822b4581a51bb3ae3e9eb57
huowutong <huowutong@outlook.com>
2017-03-31 11:18:04 +0800

    1. 添加CPU内存监控指令 2. 注册广播，接收CPU内存使用率消息

--- app/src/main/java/demo/android/com/demo2/MainActivity.java
+++ app/src/main/java/demo/android/com/demo2/MainActivity.java
@@ -1,8 +1,12 @@
 package demo.android.com.demo2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_upload;
     private Button btn_launch;
     private Button btn_clear;
+    private Button btn_start_check;
+    private Button btn_stop_check;
     private Button btn_exit;
     private Bundle bundle;
     private Intent intent;
@@ -21,23 +27,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        intent = new Intent("com.zailingtech.yunti.CAPTURE_ACTION_START");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        bundle = new Bundle();
+        bundle.putString("ID", "215552");
+
+        registerBroadcastReceiver();
+    }
+
+    private void registerBroadcastReceiver() {
+        ActionReceiver receiver = new ActionReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.zailingtech.yunti.USAGEINFO");
+        registerReceiver(receiver, filter);
+    }
+
+    private void initView() {
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_stop = (Button) findViewById(R.id.btn_stop);
         btn_upload = (Button) findViewById(R.id.btn_upload);
         btn_launch = (Button) findViewById(R.id.btn_launch);
         btn_exit = (Button) findViewById(R.id.btn_exit);
         btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_start_check = (Button) findViewById(R.id.btn_start_check);
+        btn_stop_check = (Button) findViewById(R.id.btn_stop_check);
         btn_start.setOnClickListener(this);
         btn_stop.setOnClickListener(this);
         btn_upload.setOnClickListener(this);
         btn_launch.setOnClickListener(this);
         btn_exit.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
-
-        intent = new Intent("com.zailingtech.yunti.CAPTURE_ACTION_START");
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        bundle = new Bundle();
-        bundle.putString("ID", "215552");
+        btn_start_check.setOnClickListener(this);
+        btn_stop_check.setOnClickListener(this);
     }
 
     @Override
@@ -60,12 +83,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_clear:
                 bundle.putString("action", "clear");
                 break;
+            case R.id.btn_start_check:
+                bundle.putString("action", "startCheck");
+                break;
+            case R.id.btn_stop_check:
+                bundle.putString("action", "stopCheck");
+                break;
             case R.id.btn_exit:
                 bundle.putString("action", "exit");
                 break;
         }
         intent.putExtras(bundle);
         sendBroadcast(intent);
+    }
+
+    class ActionReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle == null) {
+                return;
+            }
+            float cupUsage = bundle.getFloat("cupUsage");
+            float memoryUsage = bundle.getFloat("memoryUsage");
+            Log.e("CPU内存使用率", "CPU使用率：" + cupUsage + "%  " + "内存使用率：" + memoryUsage + "%");
+        }
     }
 
 }
